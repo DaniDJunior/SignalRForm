fcc765133d7812e1d4f5671e4c68c6544ca0e9c9
Dorval Junior <dorval.junior@acesso.io>
2017-07-16 11:55:52 -0300

    Primeira Versao

---
+++ SignalRForm.Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRForm.Server.MvcApplication" Language="C#" %>
---
+++ SignalRForm.Server/Hubs/Mensagens.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRForm.Server.Hubs
+{
+    [HubName("mensagens")]
+    public class Mensagens : Hub
+    {
+        public void CreateConnection(string nome)
+        {
+            if (CacheHelper.Exists("Client_" + nome))
+            {
+                Clients.Caller.MessageError("Usuario já existe");
+            }
+            else
+            {
+                CacheHelper.Add<string>(Context.ConnectionId, "Client_" + nome);
+                //Clients.Caller.ConnectionSucess("Chat conectado para " + nome);
+                if (CacheHelper.Exists("Client"))
+                {
+                    CacheHelper.Add<string>(CacheHelper.Get<string>("Client") + "," + nome, "Client");
+                }
+                else
+                {
+                    CacheHelper.Add<string>(nome, "Client");
+                }
+                //Clients.All.MessageNewUser(CacheHelper.Get<string>("Client"));
+            }
+        }
+    }
+}
